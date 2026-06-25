283f512e2d18098e340479456ee96b7c189d3e13
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-06-25 10:00:54 +0545

    supabase setup

---
+++ my_app/lib/Services/auth_service.dart
@@ -0,0 +1,27 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AuthService {
+  final SupabaseClient supabase = Supabase.instance.client;
+
+  Future<AuthResponse> signUp({
+    required String email,
+    required String password,
+  }) async {
+    return await supabase.auth.signUp(email: email, password: password);
+  }
+
+  Future<AuthResponse> signIn({
+    required String email,
+    required String password,
+  }) async {
+    return await supabase.auth.signUp(email: email, password: password);
+  }
+
+  Future<void> signOut() async {
+    await supabase.auth.signOut();
+  }
+
+  User? getCurrentUser() {
+    return supabase.auth.currentUser;
+  }
+}
